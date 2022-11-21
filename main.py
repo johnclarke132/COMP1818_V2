4683db8aea6f81a8247d7f7d33f897ddf07d5fe0
john132 <johnclarke132@gmail.com>
2022-11-21 08:14:36 +0000

    standardised results layout to match team

--- main.py
+++ main.py
@@ -41,14 +41,14 @@ def spam_SVM():
     # Scores
     accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
     precision = precision_score(y_true=y_test, y_pred=y_pred)
-    recall = recall_score(y_true=y_test, y_pred=y_pred)
     f1 = f1_score(y_true=y_test, y_pred=y_pred)
+    recall = recall_score(y_true=y_test, y_pred=y_pred)
     mcc = matthews_corrcoef(y_true=y_test, y_pred=y_pred)
 
     print(f"Accuracy: {accuracy}")
     print(f"Precision: {precision}")
-    print(f"Recall: {recall}")
     print(f"F1_score: {f1}")
+    print(f"Recall: {recall}")
     print(f"MCC: {mcc}")
 
 
